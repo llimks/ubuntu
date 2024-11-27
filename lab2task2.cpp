930dfcd03712d09330ec5b1679ba2c08a25c203f
Аліна П'ятецька <alinapatecka@Noutbuk-Alina.local>
2024-11-27 16:24:00 +0200

    first commit

---
+++ lab2task2.cpp
@@ -0,0 +1,24 @@
+#include<iostream> 
+#include<iomanip>
+
+int main() {
+	double num1, num2;
+
+	std::cout << "Enter the first number: ";
+	std::cin >> num1;
+	std::cout << "Enter the second number: ";
+	std::cin >> num2;
+
+	std::cout << std::fixed << std::setprecision(12);
+
+	std::cout << "Sum: " << (num1 + num2) << std::endl;
+	std::cout << "Difference: " << (num1 - num2) << std::endl;
+	std::cout << "Multiplication: " << (num1 * num2) << std::endl;
+
+	if(num2 = 0) {
+		std::cout << "Division: " << (num1 / num2) << std::endl;
+} else {
+	std::cout << "Error: division on zero!" << std::endl;
+}
+return 0;
+}
